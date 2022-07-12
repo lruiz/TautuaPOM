191f59b992b2dcefcdca34bddb3f825b641fcc6b
lruiz <larry.b.ruiz@gmail.com>
2022-07-12 15:49:43 -0500

    update plugins, upgrade default java version to 11

--- pom.groovy
+++ pom.groovy
@@ -15,187 +15,237 @@
  */
 
 project {
-  modelVersion '4.0.0'
-  groupId 'org.tautua'
-  artifactId 'tautua'
-  version '2-SNAPSHOT'
-  packaging 'pom'
+    modelVersion '4.0.0'
+    groupId 'org.tautua'
+    artifactId 'tautua'
+    version '2-SNAPSHOT'
+    packaging 'pom'
 
-  name 'Tautua POM'
-  description 'Tautua Corporate POM'
-  url 'http://www.tautua.org'
-  inceptionYear '2020'
-
-  organization {
-    name 'Tautua'
+    name 'Tautua POM'
+    description 'Tautua Corporate POM'
     url 'http://www.tautua.org'
-  }
+    inceptionYear '2020'
 
-  licenses {
-    license {
-      name 'Apache License, Version 2.0'
-      url 'http://www.apache.org/licenses/LICENSE-2.0.txt'
-      distribution 'repo'
+    organization {
+        name 'Tautua'
+        url 'http://www.tautua.org'
     }
-  }
 
-  developers {
-    developer {
-      id 'lruiz'
-      name 'Larry Ruiz'
-      email 'lruiz@tautua.org'
-      roles {
-        role 'Architect'
-        role 'Lead Developer'
-      }
-    }
-  }
-
-  scm {
-    connection 'scm:git:https://github.com/lruiz/TautuaPOM.git'
-    developerConnection 'scm:git:https://github.com/lruiz/TautuaPOM.git'
-    url 'https://github.com/lruiz/TautuaPOM'
-  }
-
-  distributionManagement {
-    repository {
-      id 'sonatype.ossrh'
-      name 'Sonatype OSSRH'
-      url 'https://oss.sonatype.org/service/local/staging/deploy/maven2'
-    }
-    snapshotRepository {
-      id 'sonatype.ossrh'
-      name 'Sonatype OSSRH'
-      url 'https://oss.sonatype.org/content/repositories/snapshots'
-    }
-  }
-
-  properties {
-    encoding 'UTF-8'
-    'project.build.sourceEncoding' '${encoding}'
-    'project.reporting.outputEncoding' '${encoding}'
-    'maven.version' '3.6.3'
-    'java.version' '1.8'
-    'maven.compiler.source' '${java.version}'
-    'maven.compiler.target' '${java.version}'
-  }
-
-  build {
-    pluginManagement {
-      plugins {
-        plugin 'org.apache.maven.plugins:maven-antrun-plugin:1.8'
-        plugin 'org.apache.maven.plugins:maven-assembly-plugin:3.1.1'
-        plugin 'org.apache.maven.plugins:maven-clean-plugin:3.1.0'
-        plugin 'org.apache.maven.plugins:maven-compiler-plugin:3.8.1'
-        plugin 'org.apache.maven.plugins:maven-dependency-plugin:3.1.1'
-        plugin 'org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M1'
-        plugin 'org.apache.maven.plugins:maven-enforcer-plugin:3.0.0-M2'
-        plugin 'org.apache.maven.plugins:maven-failsafe-plugin:3.0.0-M3'
-        plugin 'org.apache.maven.plugins:maven-gpg-plugin:1.6'
-        plugin 'org.apache.maven.plugins:maven-install-plugin:3.0.0-M1'
-        plugin 'org.apache.maven.plugins:maven-jar-plugin:3.1.2'
-        plugin 'org.apache.maven.plugins:maven-javadoc-plugin:3.1.1'
-        plugin 'org.apache.maven.plugins:maven-project-info-reports-plugin:3.0.0'
-        plugin 'org.apache.maven.plugins:maven-resources-plugin:3.1.0'
-        plugin ('org.apache.maven.plugins:maven-site-plugin:3.9.0') {
-          dependencies {
-            dependency 'net.trajano.wagon:wagon-git:2.0.4'
-          }
+    licenses {
+        license {
+            name 'Apache License, Version 2.0'
+            url 'http://www.apache.org/licenses/LICENSE-2.0.txt'
+            distribution 'repo'
         }
-        plugin 'org.apache.maven.plugins:maven-source-plugin:3.1.0'
-        plugin 'org.apache.maven.plugins:maven-surefire-plugin:3.0.0-M3'
-        plugin 'org.codehaus.mojo:license-maven-plugin:2.0.0'
-        plugin 'org.codehaus.mojo:versions-maven-plugin:2.7'
-      }
     }
 
-    plugins {
-      plugin ('org.apache.maven.plugins:maven-enforcer-plugin') {
-        executions {
-          execution {
-            id 'enforce-rules'
-            goals 'enforce'
-            configuration {
-              rules {
-                requireMavenVersion {
-                  version '${maven.version}'
-                }
-                requireJavaVersion {
-                  version '${java.version}'
-                }
-              }
+    developers {
+        developer {
+            id 'lruiz'
+            name 'Larry Ruiz'
+            email 'lruiz@tautua.org'
+            roles {
+                role 'Architect'
+                role 'Lead Developer'
             }
-          }
         }
-        dependencies {
-          dependency 'org.codehaus.mojo:extra-enforcer-rules:1.2'
-        }
-      }
     }
-  }
 
-  reporting {
-    plugins {
-      plugin {
-        groupId 'org.codehaus.mojo'
-        artifactId 'license-maven-plugin'
-        reportSets {
-          reportSet {
-            reports 'third-party-report'
-          }
-        }
-        configuration {
-          excludedScopes 'test,provided,system'
-        }
-      }
-      plugin {
-        groupId 'org.codehaus.mojo'
-        artifactId 'versions-maven-plugin'
-        reportSets {
-          reportSet {
-            reports 'dependency-updates-report',
-                    'plugin-updates-report',
-                    'property-updates-report'
-          }
-        }
-      }
+    scm {
+        connection 'scm:git:https://github.com/lruiz/TautuaPOM.git'
+        developerConnection 'scm:git:https://github.com/lruiz/TautuaPOM.git'
+        url 'https://github.com/lruiz/TautuaPOM'
     }
-  }
 
-  profiles {
-    profile {
-      id 'live'
-      build {
+    distributionManagement {
+        repository {
+            id 'sonatype.ossrh'
+            name 'Sonatype OSSRH'
+            url 'https://oss.sonatype.org/service/local/staging/deploy/maven2'
+        }
+        snapshotRepository {
+            id 'sonatype.ossrh'
+            name 'Sonatype OSSRH'
+            url 'https://oss.sonatype.org/content/repositories/snapshots'
+        }
+    }
+
+    properties {
+        encoding 'UTF-8'
+        'project.build.sourceEncoding' '${encoding}'
+        'project.reporting.outputEncoding' '${encoding}'
+        'maven.version' '3.8.2'
+        'java.version' '11'
+        'maven.compiler.source' '${java.version}'
+        'maven.compiler.target' '${java.version}'
+    }
+
+    build {
+        pluginManagement {
+            plugins {
+                plugin 'org.apache.maven.plugins:maven-antrun-plugin:3.1.0'
+                plugin 'org.apache.maven.plugins:maven-assembly-plugin:3.1.1'
+                plugin 'org.apache.maven.plugins:maven-clean-plugin:3.2.0'
+                plugin 'org.apache.maven.plugins:maven-compiler-plugin:3.10.1'
+                plugin 'org.apache.maven.plugins:maven-dependency-plugin:3.3.0'
+                plugin 'org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M2'
+                plugin 'org.apache.maven.plugins:maven-enforcer-plugin:3.1.0'
+                plugin 'org.apache.maven.plugins:maven-failsafe-plugin:3.0.0-M7'
+                plugin 'org.apache.maven.plugins:maven-gpg-plugin:3.0.1'
+                plugin 'org.apache.maven.plugins:maven-install-plugin:3.0.0-M1'
+                plugin 'org.apache.maven.plugins:maven-jar-plugin:3.2.2'
+                plugin 'org.apache.maven.plugins:maven-javadoc-plugin:3.4.0'
+                plugin 'org.apache.maven.plugins:maven-project-info-reports-plugin:3.3.0'
+                plugin 'org.apache.maven.plugins:maven-resources-plugin:3.2.0'
+                plugin('org.apache.maven.plugins:maven-site-plugin:3.12.0') {
+                    dependencies {
+                        dependency 'net.trajano.wagon:wagon-git:2.0.4'
+                    }
+                }
+                plugin 'org.apache.maven.plugins:maven-source-plugin:3.2.1'
+                plugin 'org.apache.maven.plugins:maven-surefire-plugin:3.0.0-M7'
+                plugin 'org.codehaus.mojo:license-maven-plugin:2.0.0'
+                plugin 'org.codehaus.mojo:versions-maven-plugin:2.11.0'
+                plugin 'org.jacoco:jacoco-maven-plugin:0.8.8'
+            }
+        }
+
         plugins {
-          plugin ('org.apache.maven.plugins:maven-source-plugin') {
-            executions {
-              execution {
-                id 'attach-sources'
-                phase 'verify'
-                goals 'jar'
-              }
-            }
-          }
-          plugin ('org.apache.maven.plugins:maven-javadoc-plugin') {
-            executions {
-              execution {
-                id 'attach-javadocs'
-                phase 'verify'
-                goals 'jar'
-              }
-            }
-          }
-          plugin ('org.apache.maven.plugins:maven-gpg-plugin') {
-            executions {
-              execution {
-                id 'sign-artifacts'
-                phase 'verify'
-                goals 'sign'
-              }
-            }
-          }
+            plugin 'org.apache.maven.plugins:maven-compiler-plugin'
+            plugin 'org.apache.maven.plugins:maven-jar-plugin'
+            plugin 'org.apache.maven.plugins:maven-install-plugin'
         }
-      }
     }
-  }
+
+    reporting {
+        plugins {
+            plugin {
+                groupId 'org.codehaus.mojo'
+                artifactId 'license-maven-plugin'
+                reportSets {
+                    reportSet {
+                        reports 'third-party-report'
+                    }
+                }
+                configuration {
+                    excludedScopes 'test,provided,system'
+                }
+            }
+            plugin {
+                groupId 'org.codehaus.mojo'
+                artifactId 'versions-maven-plugin'
+                reportSets {
+                    reportSet {
+                        reports 'dependency-updates-report',
+                                'plugin-updates-report',
+                                'property-updates-report'
+                    }
+                }
+            }
+        }
+    }
+
+    profiles {
+        profile {
+            id 'full'
+            build {
+                plugins {
+                    plugin('org.apache.maven.plugins:maven-enforcer-plugin') {
+                        executions {
+                            execution {
+                                id 'enforce-rules'
+                                goals 'enforce'
+                                configuration {
+                                    rules {
+                                        requireMavenVersion {
+                                            version '${maven.version}'
+                                        }
+                                        requireJavaVersion {
+                                            version '${java.version}'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        dependencies {
+                            dependency ('org.codehaus.mojo:extra-enforcer-rules:1.3') {
+                                exclusions {
+                                    exclusion 'org.eclipse.aether:aether-util'
+                                }
+                            }
+                        }
+                    }
+                    plugin 'org.apache.maven.plugins:maven-failsafe-plugin'
+                    plugin 'org.apache.maven.plugins:maven-surefire-plugin'
+                    plugin ('org.jacoco:jacoco-maven-plugin') {
+                        executions {
+                            execution {
+                                id 'prepare-agent'
+                                goals 'prepare-agent'
+                            }
+                            execution {
+                                id 'report'
+                                goals 'report'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        profile {
+            id 'live'
+            build {
+                plugins {
+                    plugin('org.apache.maven.plugins:maven-source-plugin') {
+                        executions {
+                            execution {
+                                id 'attach-sources'
+                                phase 'verify'
+                                goals 'jar'
+                            }
+                        }
+                    }
+                    plugin('org.apache.maven.plugins:maven-javadoc-plugin') {
+                        executions {
+                            execution {
+                                id 'attach-javadocs'
+                                phase 'verify'
+                                goals 'jar'
+                            }
+                        }
+                    }
+                    plugin('org.apache.maven.plugins:maven-gpg-plugin') {
+                        executions {
+                            execution {
+                                id 'sign-artifacts'
+                                phase 'verify'
+                                goals 'sign'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        profile {
+            id 'coverage'
+            build {
+                plugins {
+                    plugin ('org.jacoco:jacoco-maven-plugin') {
+                        executions {
+                            execution {
+                                id 'pre-tests'
+                                goals 'prepare-agent'
+                            }
+                            execution {
+                                id 'post-tests'
+                                goals 'report'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
